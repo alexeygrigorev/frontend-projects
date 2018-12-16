555b31e28bed76d6dbf076481c739de69a28fa31
Alexey Grigorev <alexey.s.grigoriev@gmail.com>
2018-12-16 21:44:12 +0100

    part 2

--- src/components/BusinessList/BusinessList.js
+++ src/components/BusinessList/BusinessList.js
@@ -5,15 +5,15 @@ import Business from '../Business/Business';
 
 
 class BusinessList extends React.Component {
+
   render() {
+    const businesses = this.props.businesses.map(business => 
+      <Business business={business} />
+    );
+
     return (
       <div className="BusinessList">
-        <Business />
-        <Business />
-        <Business />
-        <Business />
-        <Business />
-        <Business />
+        {businesses}
       </div>
     );
   }
